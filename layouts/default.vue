cd6fad6b4d86f0eae4b0dca7fa79b6f9265ebff1
h_yamasaki <h_yamasaki@yumemi.co.jp>
2019-04-17 16:17:40 +0900

    不要なコメント削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,28 +6,5 @@
 
 <script>
   export default {
-    // data() {
-    //   return {
-    //       clipped: false,
-    //       drawer: false,
-    //       fixed: false,
-    //       items: [
-    //         {
-    //           icon: ‘apps’,
-    //           title: Welcome’,
-    //           to: ‘/’
-    //         },
-    //         {
-    //           icon: ‘bubble_chart’,
-    //           title: ‘Inspire’,
-    //           to: ‘/inspire’
-    //         }
-    //       ],
-    //         miniVariant: false,
-    //         right: true,
-    //         rightDrawer: false,
-    //         title: ‘Vuetify.js’
-    //   }
-    // }
   }
 </script>
